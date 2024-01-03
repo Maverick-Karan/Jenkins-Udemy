b95f63de520181d6c9022b6fd3e71992fa1116db
Karan <Karan@gmail.com>
2024-01-03 03:09:01 +0000

    undo push.sh changes

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -7,8 +7,6 @@ echo "********************"
 IMAGE="maven-project" 
 
 echo "*** Logging In ***"
-docker login
-#using only docker login will also work as prod-machine has now saved the credentials, else create secret file in Jenkins console with pass
 #docker login -u maverick8266 -p $PASS 
 echo "*** Tagging image ***"
 docker tag $IMAGE:$BUILD_TAG maverick8266/$IMAGE:$BUILD_TAG 
