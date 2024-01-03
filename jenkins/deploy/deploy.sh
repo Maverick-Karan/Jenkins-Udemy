913b920426c7fed3361dce9bf603865ef0ba2e08
Karan <Karan@gmail.com>
2024-01-03 04:35:20 +0000

    chmod in deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,8 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 #echo $PASS >> /tmp/.auth
 
+chmod 400 prod
+
 scp -i prod /tmp/.auth prod-user@54.162.183.181:/tmp/.auth
 scp -i prod publish.sh prod-user@54.162.183.181:/tmp/publish.sh
 
