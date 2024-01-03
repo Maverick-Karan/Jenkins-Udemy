9ab014f5d27a505db887fda04bb0c7aca70d1f54
Karan <Karan@gmail.com>
2024-01-03 02:54:58 +0000

    fixed docker login in push.sh

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -7,7 +7,9 @@ echo "********************"
 IMAGE="maven-project" 
 
 echo "*** Logging In ***"
-docker login -u maverick8266 -p $PASS 
+docker login
+#using only docker login will also work as prod-machine has now saved the credentials, else create secret file in Jenkins console with pass
+#docker login -u maverick8266 -p $PASS 
 echo "*** Tagging image ***"
 docker tag $IMAGE:$BUILD_TAG maverick8266/$IMAGE:$BUILD_TAG 
 echo "*** Pushing image ***" 
