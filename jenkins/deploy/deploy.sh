2b61b9b807771317df821337feb5595a4f3c6114
Karan <Karan@gmail.com>
2024-01-03 04:11:24 +0000

    testing Prod IP manual

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,8 +4,8 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 #echo $PASS >> /tmp/.auth
 
-scp -i prod /tmp/.auth prod-user@$PROD_IP:/tmp/.auth
-scp -i prod publish.sh prod-user@$PROD_IP:/tmp/publish.sh
+scp -i prod /tmp/.auth prod-user@54.162.183.181:/tmp/.auth
+scp -i prod publish.sh prod-user@54.162.183.181:/tmp/publish.sh
 
-ssh -i prod prod-user@$PROD_IP "/tmp/publish.sh"
+ssh -i prod prod-user@54.162.183.181 "/tmp/publish.sh"
 
