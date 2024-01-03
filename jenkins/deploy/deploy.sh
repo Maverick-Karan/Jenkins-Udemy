799c54b91e09d8b6e17abb7e9063503c3c583bdf
Karan <Karan@gmail.com>
2024-01-03 03:35:16 +0000

    prod location in deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,8 +4,8 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 #echo $PASS >> /tmp/.auth
 
-scp -i /home/ubuntu/jenkins/pipeline/jenkins/deploy/prod /tmp/.auth prod-user@$PROD_IP:/tmp/.auth
-scp -i /home/ubuntu/jenkins/pipeline/jenkins/deploy/prod ./jenkins/deploy/publish.sh prod-user@$PROD_IP:/tmp/publish.sh
+scp -i prod /tmp/.auth prod-user@$PROD_IP:/tmp/.auth
+scp -i prod publish.sh prod-user@$PROD_IP:/tmp/publish.sh
 
-ssh -i /home/ubuntu/jenkins/pipeline/jenkins/deploy/prod prod-user@$PROD_IP "/tmp/publish.sh"
+ssh -i prod prod-user@$PROD_IP "/tmp/publish.sh"
 
